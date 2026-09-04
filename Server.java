a0193fe7e424cb9c5f76cd0464922574d63c9fe9
Andrew Liu <andrewliu3203@gmail.com>
2026-09-04 01:39:30 -0700

    functioning server

--- Server.java
+++ Server.java
@@ -3,11 +3,31 @@ import java.io.*;
 
 public class Server{
 
-    private int PORT = 6666;
+    private static final int PORT = 6380;
 
     public static void main(String[] args){
-        
-        Serverso
+        try(ServerSocket ss = new ServerSocket(PORT)){
+            while(true){
+                try{
+                    Socket s = ss.accept();
+                    Thread.ofVirtual().start(() -> handle(s));
+                }
+                catch(Exception e){
+                    System.out.println("accept failed: " + e);
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println("error " + e);
+        }
+    }
 
+    private static void handle(Socket s){
+        try(s){
+            System.out.println("response: " + s);
+        }
+        catch(Exception e){
+            System.out.println("error in handling " + e);
+        }
     }
 }
