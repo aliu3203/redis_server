1125ed29f9514304ed120a3c39fbebd90d777edf
Andrew Liu <andrewliu3203@gmail.com>
2026-09-04 23:28:05 -0700

    I/O functionality

--- Server.java
+++ Server.java
@@ -23,8 +23,19 @@ public class Server{
     }
 
     private static void handle(Socket s){
-        try(s){
-            System.out.println("response: " + s);
+        try(s;
+            InputStream in = s.getInputStream();
+            OutputStream out = s.getOutputStream();
+        ){
+            byte[] buf = new byte[256];
+            int n;
+            while((n = in.read(buf)) != -1){
+                if(n > 0){
+                    System.out.println("request " + new String(buf, 0, n));
+                }
+            }
+
+            
         }
         catch(Exception e){
             System.out.println("error in handling " + e);
