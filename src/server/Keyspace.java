ccc58dfdd725e34ac7bfe946fe9d2b6a2804d51e
Andrew Liu <andrewliu3203@gmail.com>
2026-09-09 10:50:54 -0700

    initial skeleton

---
+++ src/server/Keyspace.java
@@ -0,0 +1,15 @@
+package server;
+
+public class Keyspace{
+    private final Map<String, byte[]> data = new Hashmap<>();
+
+    public byte[] get(String key){
+
+    }
+    public void set(String key, byte[] value){
+
+    }
+    public long incr(String key){
+
+    }
+}
