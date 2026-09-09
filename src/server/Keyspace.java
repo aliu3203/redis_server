8f01c22dfc853f2b32d1aa29aace2fe4f972230d
Andrew Liu <andrewliu3203@gmail.com>
2026-09-09 11:14:25 -0700

    added keyspace get + set

--- src/server/Keyspace.java
+++ src/server/Keyspace.java
@@ -1,15 +1,25 @@
 package server;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.nio.charset.StandardCharsets;
+
 public class Keyspace{
-    private final Map<String, byte[]> data = new Hashmap<>();
+    private final Map<String, byte[]> data = new HashMap<>();
 
     public byte[] get(String key){
-
+        return data.get(key);
     }
     public void set(String key, byte[] value){
-
+        data.put(key, value);
     }
-    public long incr(String key){
 
+    // No handling of LONG.MAX_VALUE
+    public long incr(String key){
+        byte[] curr = data.get(key);
+        long n = (curr == null) ? 0 : Long.parseLong(new String(cur, StandardCharsets.ISO_8859_1));
+        n += 1;
+        data.put(key, Long.toString(n).getBytes(StandardCharsets.ISO_8859_1));
+        return n;
     }
 }
